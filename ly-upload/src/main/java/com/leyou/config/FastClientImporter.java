4bae06b5367fadc9780c2318b5c3f685b5d3ad4e
tiancixiong <tiancixiong@163.com>
2019-11-03 10:50:55 +0800

    整合FastDFS，测试其上传图片功能

---
+++ ly-upload/src/main/java/com/leyou/config/FastClientImporter.java
@@ -0,0 +1,19 @@
+package com.leyou.config;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * @Author: TianCi.Xiong
+ * @Description: FastDFS配置
+ * @Date: Created in 2019-11-03 8:47
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastClientImporter {
+}
---
+++ ly-upload/src/test/java/FdfsTest.java
@@ -0,0 +1,53 @@
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.domain.ThumbImageConfig;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import com.leyou.LyUploadServiceApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * @Author: TianCi.Xiong
+ * @Description: FastDFS测试
+ * @Date: Created in 2019-11-03 8:55
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LyUploadServiceApplication.class)
+public class FdfsTest {
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private ThumbImageConfig thumbImageConfig;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        File file = new File("G:\\test\\baby.png");
+        // 上传并生成缩略图
+        StorePath storePath = this.storageClient.uploadFile(new FileInputStream(file), file.length(), "png", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+    }
+
+    @Test
+    public void testUploadAndCreateThumb() throws FileNotFoundException {
+        File file = new File("G:\\test\\baby.png");
+        // 上传并且生成缩略图
+        StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(new FileInputStream(file), file.length(), "png", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+        // 获取缩略图路径
+        String path = thumbImageConfig.getThumbImagePath(storePath.getPath());
+        System.out.println(path);
+    }
+}
