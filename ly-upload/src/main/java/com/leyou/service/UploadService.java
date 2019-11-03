d08816a6db2276699b39ad4277eddc3bc20aa4e0
tiancixiong <tiancixiong@163.com>
2019-11-03 12:06:31 +0800

    更改upload-service中的上传逻辑，图片保存在本地->图片保存在虚拟机FastDFS中

--- ly-upload/src/main/java/com/leyou/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/service/UploadService.java
@@ -1,14 +1,17 @@
 package com.leyou.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.controller.UploadController;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,6 +26,9 @@ public class UploadService {
 
     // 支持的文件类型
     private static final List<String> suffixes = Arrays.asList("image/png", "image/jpeg");
+
+    @Autowired
+    FastFileStorageClient storageClient;
 
     /**
      * 上传图片功能
@@ -45,22 +51,15 @@ public class UploadService {
                 logger.info("上传失败，文件内容不符合要求");
                 return null;
             }
-            // 2、保存图片
-            // 2.1、生成保存目录
-            File dir = new File("G:\\leyou\\upload");
-            if (!dir.exists()) {
-                // 创建目录
-                dir.mkdirs();
-            }
-            // 2.2、保存图片
-            file.transferTo(new File(dir, file.getOriginalFilename()));
 
-            // 2.3、拼接图片地址
-            String url = "http://image.leyou.com/upload/" + file.getOriginalFilename();
-            // 返回图片地址 http://image.leyou.com/upload/+文件名
-            return url;
+            // 2、将图片上传到FastDFS
+            // 2.1、获取文件后缀名
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+            // 2.2、上传
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
+            // 2.3、返回完整路径
+            return "http://image.leyou.com/" + storePath.getFullPath();
         } catch (Exception e) {
-            // 出现异常，返回null，Controller层判断
             return null;
         }
     }
