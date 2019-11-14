df951d3fecfd54aa838865e27697716b91199805
tiancixiong <tiancixiong@163.com>
2019-11-14 12:10:10 +0800

    实现判断用户登录状态功能

--- ly-auth/ly-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
+++ ly-auth/ly-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
@@ -1,16 +1,16 @@
 package com.leyou.auth.controller;
 
 import com.leyou.auth.config.JwtProperties;
+import com.leyou.auth.entity.UserInfo;
 import com.leyou.auth.service.AuthService;
+import com.leyou.auth.utils.JwtUtils;
 import com.leyou.common.utils.CookieUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,7 +27,7 @@ public class AuthController {
     private AuthService authService;
 
     @Autowired
-    private JwtProperties prop;
+    private JwtProperties properties;
 
     /**
      * 登录授权
@@ -48,8 +48,37 @@ public class AuthController {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         // 将token写入cookie,并指定httpOnly为true，防止通过JS获取和修改
-        CookieUtils.setCookie(request, response, prop.getCookieName(),
-                token, prop.getCookieMaxAge(), null, true);
+        CookieUtils.setCookie(request, response, properties.getCookieName(),
+                token, properties.getCookieMaxAge(), null, true);
         return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 验证用户信息
+     *
+     * @param token
+     * @return
+     */
+    @GetMapping("/verify")
+    public ResponseEntity<UserInfo> verifyUser(
+            @CookieValue("LY_TOKEN") String token,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        try {
+            // 从token中解析token信息
+            UserInfo userInfo = JwtUtils.getInfoFromToken(token, this.properties.getPublicKey());
+            // 解析成功要重新刷新token
+            token = JwtUtils.generateToken(userInfo, this.properties.getPrivateKey(), this.properties.getExpire());
+            // 更新cookie中的token
+            CookieUtils.setCookie(request, response, this.properties.getCookieName(), token, this.properties.getCookieMaxAge());
+
+            // 解析成功返回用户信息
+            return ResponseEntity.ok(userInfo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 出现异常则，响应500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
