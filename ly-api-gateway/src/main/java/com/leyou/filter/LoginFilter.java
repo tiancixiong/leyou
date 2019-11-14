33f1ba2ff6a9aa6d657c8236b51470e5a61f186e
tiancixiong <tiancixiong@163.com>
2019-11-14 13:00:19 +0800

    实现网关的登录拦截器并配置白名单

---
+++ ly-api-gateway/src/main/java/com/leyou/filter/LoginFilter.java
@@ -0,0 +1,82 @@
+package com.leyou.filter;
+
+import com.leyou.auth.utils.JwtUtils;
+import com.leyou.common.utils.CookieUtils;
+import com.leyou.config.FilterProperties;
+import com.leyou.config.JwtProperties;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: TianCi.Xiong
+ * @Description:
+ * @Date: Created in 2019-11-14 12:22
+ */
+@Component
+@EnableConfigurationProperties({JwtProperties.class, FilterProperties.class})
+public class LoginFilter extends ZuulFilter {
+    @Autowired
+    private JwtProperties jwtProperties;
+    @Autowired
+    private FilterProperties filterProperties;
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 5;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        // 获取上下文
+        RequestContext ctx = RequestContext.getCurrentContext();
+        // 获取request
+        HttpServletRequest req = ctx.getRequest();
+        // 获取路径
+        String requestURI = req.getRequestURI();
+        // 判断白名单
+        // 遍历允许访问的路径
+        for (String path : this.filterProperties.getAllowPaths()) {
+            // 然后判断是否是符合
+            if (requestURI.startsWith(path)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        // 获取上下文
+        RequestContext context = RequestContext.getCurrentContext();
+        // 获取request
+        HttpServletRequest request = context.getRequest();
+        // 获取token
+        String token = CookieUtils.getCookieValue(request, this.jwtProperties.getCookieName());
+        // 校验
+        try {
+            // 校验通过什么都不做，即放行
+            JwtUtils.getInfoFromToken(token, this.jwtProperties.getPublicKey());
+        } catch (Exception e) {
+            // 校验出现异常，返回403
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(HttpStatus.FORBIDDEN.value());
+        }
+        return null;
+    }
+}
